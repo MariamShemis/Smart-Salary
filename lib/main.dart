b00e83317a45ebeaf9a87225929afb7ee7a0e833
Mariam Shemis <shemesmariem@example.com>
2026-07-12 21:26:03 +0300

    add firebase in project1

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:smart_salary/features/main_layout/daily_reports/data/cubit/daily_reports_cubit.dart';
@@ -5,7 +6,13 @@ import 'package:smart_salary/features/main_layout/home/data/cubit/home_cubit.dar
 import 'package:smart_salary/features/main_layout/salary_calculator/data/cubit/salary_cubit.dart';
 import 'package:smart_salary/smart_salary.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MultiBlocProvider(
       providers: [
